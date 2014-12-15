87b7b7f2ad9d9525ae83019d73f195c5ed4f17c5
tsarzju <tsar.zju@gmail.com>
2014-12-15 23:19:57 +0800

    fix bug

--- script/config.js
+++ script/config.js
@@ -1,15 +1,8 @@
 define(['fs'], function(fs){
   var config = {};
   var hasError = false;
-  try {
-    config = JSON.parse(fs.readFileSync('config.ini', 'utf8'));
-    config.fullFormat = config.dateFormat + ' ' + config.timeFormat + config.lastFormat;
-  } catch(error) {
-    hasError = true;
-  }
-  if (config.encoding === undefined || hasError) {
-    config.encoding = 'utf8';
-  }
+  config = JSON.parse(fs.readFileSync('config.ini', 'utf8'));
+  config.fullFormat = config.dateFormat + ' ' + config.timeFormat + config.lastFormat;
 
   return config;
 });
