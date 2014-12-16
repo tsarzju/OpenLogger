59a4932a053aac3327bd72eee835032f001f635b
qian_z <qian_z@worksap.co.jp>
2014-12-16 18:58:49 +0800

    fix bug and add README.md

--- script/file.js
+++ script/file.js
@@ -1,14 +1,15 @@
 define(['fs', 'iconv-lite', 'config'], function(fs, iconv, config){
-  function open(path, callback) {
+  function open(path, callback, encoding) {
     fs.readFile(path, function(error, contents) {
       if (error) return;
-      var data = iconv.decode(contents, config.encoding);
+      var data = iconv.decode(contents, encoding);
       callback(path, data);
     });
   }
 
-  function save(path, contents) {
-    fs.writeFile(path, contents);
+  function save(path, contents, encoding) {
+    var data = iconv.encode(contents, encoding);
+    fs.writeFile(path, data);
   }
 
   return {
