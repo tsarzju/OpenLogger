4af37cb67a55847b15035bdf986fc3f91d87c4a9
qian_z <qian_z@worksap.co.jp>
2014-12-18 15:53:52 +0800

    update config.ini file location

--- script/config.js
+++ script/config.js
@@ -1,7 +1,14 @@
-define(['fs'], function(fs){
+define(['fs', 'path'], function(fs, path){
   var config = {};
   var hasError = false;
-  config = JSON.parse(fs.readFileSync('config.ini', 'utf8'));
+  var execPath = path.dirname( process.execPath );
+  var fileName = 'config.ini';
+  var exists = fs.existsSync(execPath+'/'+fileName);
+  if (exists) {
+    fileName = execPath+'/'+fileName;
+  }
+
+  config = JSON.parse(fs.readFileSync(fileName, 'utf8'));
 
   return config;
 });
